d0961bcf87f57d47a5b7df9232a8ff14ebe746ca
Florian MARGAINE <florian@margaine.com>
2014-11-27 15:13:02 +0100

    Adds the CommerceTaxRateAmount entity and friends

--- modules/tax/src/Form/CommerceTaxRateAmountForm.php
+++ modules/tax/src/Form/CommerceTaxRateAmountForm.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Contains \Drupal\commerce_tax\Form\CommerceTaxRateForm.
+ * Contains \Drupal\commerce_tax\Form\CommerceTaxRateAmountForm.
  */
 
 namespace Drupal\commerce_tax\Form;
@@ -11,8 +11,16 @@ use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\commerce_tax\Entity\CommerceTaxRate;
 
-class CommerceTaxRateForm extends EntityForm {
+class CommerceTaxRateAmountForm extends EntityForm {
+
+  /**
+   * The tax rate amount storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $taxRateAmountStorage;
 
   /**
    * The tax rate storage.
@@ -22,12 +30,15 @@ class CommerceTaxRateForm extends EntityForm {
   protected $taxRateStorage;
 
   /**
-   * Creates a CommerceTaxRateForm instance.
+   * Creates a CommerceTaxRateAmountForm instance.
    *
+   * @param \Drupal\Core\Entity\EntityStorageInterface $tax_rate_amount_storage
+   *   The tax rate amount storage.
    * @param \Drupal\Core\Entity\EntityStorageInterface $tax_rate_storage
    *   The tax rate storage.
    */
-  public function __construct(EntityStorageInterface $tax_rate_storage) {
+  public function __construct(EntityStorageInterface $tax_rate_amount_storage, EntityStorageInterface $tax_rate_storage) {
+    $this->taxRateAmountStorage = $tax_rate_amount_storage;
     $this->taxRateStorage = $tax_rate_storage;
   }
 
@@ -38,7 +49,7 @@ class CommerceTaxRateForm extends EntityForm {
     /** @var \Drupal\Core\Entity\EntityManagerInterface $entity_manager */
     $entity_manager = $container->get('entity.manager');
 
-    return new static($entity_manager->getStorage('commerce_tax_rate'));
+    return new static($entity_manager->getStorage('commerce_tax_rate_amount'), $entity_manager->getStorage('commerce_tax_rate'));
   }
 
   /**
@@ -88,38 +99,17 @@ class CommerceTaxRateForm extends EntityForm {
    * Validates the id field.
    */
   public function validateId(array $element, FormStateInterface &$form_state, array $form) {
-    $tax_rate = $this->getEntity();
+    $tax_rate_amount = $this->getEntity();
     $id = $element['#value'];
     if (!preg_match('/[a-z_]+/', $id)) {
       $form_state->setError($element, $this->t('The machine name must be in lowercase, underscore-separated letters only.'));
     }
-    elseif ($tax_rate->isNew()) {
-      $loaded_tax_rates = $this->taxRateStorage->loadByProperties(array(
+    elseif ($tax_rate_amount->isNew()) {
+      $loaded_tax_rate_amounts = $this->taxRateAmountStorage->loadByProperties(array(
         'id' => $id,
       ));
-      if ($loaded_tax_rates) {
+      if ($loaded_tax_rate_amounts) {
         $form_state->setError($element, $this->t('The machine name is already in use.'));
-      }
-    }
-  }
-
-  /**
-   * Validates that there is only one default per tax type.
-   */
-  public function validateDefaultness(array $element, FormStateInterface &$form_state, array $form) {
-    $tax_rate = $this->getEntity();
-    $default = $element['#value'];
-    if ($default) {
-      $loaded_tax_rates = $this->taxRateStorage->loadByProperties(array(
-        'type' => $form_state->getValue('type'),
-      ));
-      foreach ($loaded_tax_rates as $rate) {
-        if ($rate->getId() !== $tax_rate->getId() && $rate->isDefault()) {
-          $form_state->setError($element, $this->t('Tax rate %label is already the default.', array(
-            '%label' => $rate->label(),
-          )));
-          break;
-        }
       }
     }
   }
@@ -128,27 +118,27 @@ class CommerceTaxRateForm extends EntityForm {
    * {@inheritdoc}
    */
   public function save(array $form, FormStateInterface $form_state) {
-    $tax_rate = $this->entity;
+    $tax_rate_amount = $this->entity;
 
     try {
-      $tax_rate->save();
+      $tax_rate_amount->save();
       drupal_set_message($this->t('Saved the %label tax rate.', array(
-        '%label' => $tax_rate->label(),
+        '%label' => $tax_rate_amount->label(),
       )));
 
       try {
-        $tax_type = $this->taxTypeStorage->load($tax_rate->getType());
-        if (!$tax_type->hasRate($tax_rate)) {
-          $tax_type->addRate($tax_rate);
-          $tax_type->save();
+        $tax_rate = $this->taxRateStorage->load($tax_rate_amount->getRate());
+        if (!$tax_rate->hasAmount($tax_rate_amount)) {
+          $tax_rate->addAmount($tax_rate_amount);
+          $tax_rate->save();
         }
 
-        $form_state->setRedirect('entity.commerce_tax_rate.list', array(
-          'commerce_tax_type' => $tax_type->getId(),
+        $form_state->setRedirect('entity.commerce_tax_rate_amount.list', array(
+          'commerce_tax_rate' => $tax_rate->getId(),
         ));
       }
       catch (\Exception $e) {
-        drupal_set_message($this->t('The %label tax type was not saved.', array(
+        drupal_set_message($this->t('The %label tax rate was not saved.', array(
           '%label' => $tax_type->label(),
         )));
         throw $e;
@@ -156,7 +146,7 @@ class CommerceTaxRateForm extends EntityForm {
 
     }
     catch (\Exception $e) {
-      drupal_set_message($this->t('The %label tax rate was not saved.', array(
+      drupal_set_message($this->t('The %label tax rate amount was not saved.', array(
         '%label' => $tax_rate->label()
       )), 'error');
       $this->logger('commerce_tax')->error($e);
